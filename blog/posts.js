a679add55d76f8cf2bdb171cc96cc4c64a670bf3
毛遵杰 (Zaki) <1099298078@qq.com>
2026-01-31 12:49:28 +0800

    博客增加

--- blog/posts.js
+++ blog/posts.js
@@ -1,6 +1,14 @@
-// blog/posts.js
+
 
 const blogPosts = [
+    {
+        url: "blog/ohenry.html",
+        date: "06.12.2022",
+        title_en: "O. Henry's Tragicomic World: Reflections on Reading",
+        title_zh: "【读书随感】欧·亨利笔下的悲喜世界",
+        tag_en: "Reading",
+        tag_zh: "读书随笔"
+    },
     {
         url: "blog/soul.html",
         date: "01.23.2021",
